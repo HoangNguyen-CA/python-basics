dd6997b9da7861bf1aad98bfb41b31b40ac7e543
Hoang <hoang.viet.nguyen@outlook.com>
2020-09-08 20:48:17 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+person = {
+    'firstname': 'Joe',
+    'lastname': 'Mama'
+}
+
+# formatted string
+print(f'wow {person}')
+print(person.items())
+print(len(person))
+
+'''
+FUNCTIONS
+'''
+
+
+def say_hello(name):
+    print(name)
+
+
+def get_sum(num1, num2):
+    temp_sum = num1 + num2
+    return temp_sum
+
+
+print(get_sum(1, 5))
+
+
+'''
+LOOPS
+'''
+
